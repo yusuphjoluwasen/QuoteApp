ab93134bea373542e8ee54760958b10dd357d9f5
Oluwaseun Jimoh <j.oluwaseun@verifyme.ng>
2024-10-24 14:10:07 +0100

    adding Quote App to a remote repository

---
+++ src/Constants/Texts.ts
@@ -0,0 +1,15 @@
+export default {
+  Login: 'Login',
+  Splash: 'Splash',
+  DetailScreen: 'DetailScreen',
+  HomeScreen: 'HomeScreen',
+  ProfileScreen: 'ProfileScreen',
+  MainBottomTab: 'MainBottomTab',
+  AuthorScreen: 'AuthorScreen',
+  TagsScreen: 'TagsScreen',
+  RandomQuoteScreen: 'RandomQuoteScreen',
+  QuoteByAuthorScreen: 'QuoteByAuthorScreen',
+  QuoteByTagScreen: 'QuoteByTagScreen',
+  TagsDetailScreen: 'TagsDetailScreen',
+  QuoteDetailsScreen: 'QuoteDetailsScreen',
+}
